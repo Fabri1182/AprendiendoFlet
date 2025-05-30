0206038fb82aa14fb9e574a6f901c1bb8efee58d
Fabri1182 <electronica.deetel@gmail.com>
2025-05-30 13:07:53 +0000

    Otro avance de la clase 3

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # Aprendiendo Flet
 
-Estoy aprendiendo Flet desde video "Curso Completo de Flet" en youtube del canal de "Código Espinoza"
+Estoy aprendiendo Flet desde video "Curso Completo de Flet" en youtube del canal de "Código Espinoza".
 
-Este curso dura 4h30, espero aprender a crear interfaces gráficas sencillas de aplicaciones de escritorio,
-web y para telefono con python
+Este curso dura 4h30, espero aprender a crear interfaces gráficas sencillas de aplicaciones de escritorio, web y para telefono con python.
 
 Contenido del curso:
 app1, se practica con salida de texto en pantalla y botones.
-app2, se trabaja con filas y columnas
-app3, ahora praticamos con textfile y con listview, en conclusion trabajamos con listas. Nos quedamos en... video stopped at 22 minutes and 35 seconds.
+app2, se trabaja con filas y columnas.
+app3, ahora praticamos con textfile y con listview, en conclusion trabajamos con listas. Nos quedamos en... video stopped at 27 minutes and 10 seconds.
--- app3.py
+++ app3.py
@@ -7,11 +7,51 @@ def main(page: ft.Page):
     #page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.title = "App de lista de tareas"
 
-    titulo = ft.Text(value="Lista de Tareas con Flet", size=30, color=ft.Colors.WHITE,
-                     weight=ft.FontWeight.BOLD)
+    #titulo = ft.Text(value="Lista de Tareas con Flet", size=30, color=ft.Colors.WHITE,
+    #                 weight=ft.FontWeight.BOLD)
+
+    titulo = ft.Stack(
+            [
+                ft.Text(
+                    spans=[
+                        ft.TextSpan(
+                            "Listador de Tareas",
+                            ft.TextStyle(
+                                size=40,
+                                weight=ft.FontWeight.BOLD,
+                                foreground=ft.Paint(
+                                    color=ft.Colors.BLUE_700,
+                                    stroke_width=6,
+                                    style=ft.PaintingStyle.STROKE,
+                                ),
+                            ),
+                        ),
+                    ],
+                ),
+                ft.Text(
+                    spans=[
+                        ft.TextSpan(
+                            "Listador de Tareas",
+                            ft.TextStyle(
+                                size=40,
+                                weight=ft.FontWeight.BOLD,
+                                color=ft.Colors.GREY_300,
+                            ),
+                        ),
+                    ],
+                ),
+            ]
+        )
     
     def seleccionar_tarea(e):
-        pass
+        seleccionadas = [t.title.value for t in tareas if t.leading.value]
+        tareas_seleccionadas.value = "Tareas seleccionadas: " + ", ".join(seleccionadas)
+        page.update()
+
+    def actualizar_lista():
+        lista_tareas.controls.clear()
+        lista_tareas.controls.extend(tareas)
+        page.update()
 
     def agregar_tarea(e):
         if campo_tarea.value:
@@ -19,15 +59,18 @@ def main(page: ft.Page):
                 title=ft.Text(campo_tarea.value),
                 leading=ft.Checkbox(on_change=seleccionar_tarea)
             )
+        tareas.append(tarea)         # Agrega la tarea a la lista
+        campo_tarea.value = ""       # Limpia el campo de texto
+        actualizar_lista()           # Actualiza la lista en la interfaz
     
     campo_tarea = ft.TextField(hint_text="Ingrese una nueva tarea")
     boton_agregar = ft.FilledButton(text="Agregar Tarea", on_click=agregar_tarea)
-
+    lista_tareas = ft.ListView(expand=True, spacing=5)
     tareas = []
 
     tareas_seleccionadas = ft.Text(value="", size=20, 
                                    weight=ft.FontWeight.BOLD)
     
-    page.add(titulo, campo_tarea, boton_agregar, tareas_seleccionadas)
+    page.add(titulo, campo_tarea, boton_agregar, lista_tareas, tareas_seleccionadas)
 
 ft.app(target=main, view=ft.WEB_BROWSER)
